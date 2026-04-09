de0fba35d2cdd3d9459cbd2d543a71478d2644eb
McBryan <bryanokononfua@gmail.com>
2026-04-09 22:27:39 +0100

    Next Commit

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import QuestionImg from '../assets/question-mark.png';
+import { paragraph } from '../texts/description.js';
 
 const Desc = () => {
     return (
-        <section className='desc d-flex justify-content-center align-items-center'>
+        <section className='desc d-flex flex-column gap-5 justify-content-center h-100 align-items-center'>
 
-            <div className="desc-heading text-center fw-bold w-100 h-100 text-color">
-                <h1 className='curve '>HOW DOES IT</h1>
-                <h1>WORK?</h1>
+            <div>
+                <div className="desc-heading d-flex justify-content-evenly gap-5 text-center h-100 fw-bold text-color">
+                    <div>
+                        <h1 className='curve '>HOW DOES IT</h1>
+                        <h1>WORK?</h1>
+                    </div>
+                    <img
+                        src={QuestionImg}
+                        alt="question-img"
+                        className="d-none d-md-block"
+                    />
+                </div>
+
+                <div className="container h-75 mt-3">
+                    <p className='fs-3'>{paragraph}</p>
+                </div>
+            </div>
+
+            <div>
+                <div className="benefits">
+                    <div className="text-color"></div>
+                </div>
             </div>
 
         </section>
